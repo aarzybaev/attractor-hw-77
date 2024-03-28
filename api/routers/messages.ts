57a7a69673c0e41ac63f8c030bf775c8b2dfcfff
Askhad Arzybaev <aarzybaev@gmail.com>
2024-03-28 12:53:52 +0600

    created routes file

---
+++ api/routers/messages.ts
@@ -0,0 +1,32 @@
+import express from 'express';
+import {imagesUpload} from "../multer";
+import {MessageWithoutId} from "../types";
+import fileDb from "../fileDb";
+
+const messagesRouter = express.Router();
+
+messagesRouter.get('/', async (req, res) => {
+    const messages = await fileDb.getItems();
+    return res.json(messages);
+});
+
+messagesRouter.post('', imagesUpload.single('image'), async (req, res) => {
+
+    const author = req.body.author;
+    const message = req.body.message;
+
+    if (message.trim()) {
+        const messageData: MessageWithoutId = {
+            author: author.trim() ? author : null,
+            message,
+            image: req.file ? req.file.filename : null,
+        };
+        const messageItem = await fileDb.addItem(messageData);
+        return res.json(messageItem);
+    } else {
+        return res.status(404).json({"error": "Message must be present in the request"});
+    }
+
+});
+
+export default messagesRouter;
