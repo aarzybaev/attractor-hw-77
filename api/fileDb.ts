d35c86507f8a359c6fcb173d2e10bf9ae76ad25c
Askhad Arzybaev <aarzybaev@gmail.com>
2024-03-28 12:56:30 +0600

    set fileDb

---
+++ api/fileDb.ts
@@ -0,0 +1,39 @@
+import {promises as fs} from 'fs';
+import crypto from 'crypto';
+import {Message, MessageWithoutId} from "./types";
+
+
+const filename = './db.json';
+let data: Message[] = [];
+
+const fileDb = {
+  async init() {
+    try {
+      const fileContents = await fs.readFile(filename);
+      data = JSON.parse(fileContents.toString());
+    }
+    catch (e) {
+      data = [];
+    }
+  },
+  async getItems() {
+    return data;
+  },
+  async getItemById(id: string) {
+    return data.find(product => product.id === id);
+  },
+  async addItem (item: MessageWithoutId) {
+    const message: Message = {
+      id: crypto.randomUUID(),
+      ...item,
+    };
+    data.push(message);
+    await this.save();
+    return message;
+  },
+  async save() {
+    await fs.writeFile(filename, JSON.stringify(data, null, 2));
+  }
+}
+
+export default fileDb;
