6b50d00e8010717d4bc7484f93814c268760a7b2
Askhad Arzybaev <aarzybaev@gmail.com>
2024-03-29 12:38:06 +0600

    putted routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,23 @@
+import AppToolbar from './components/UI/AppToolbar/AppToolbar.tsx';
+import { Container, Typography } from '@mui/material';
+import { Route, Routes } from 'react-router-dom';
+import Messages from './features/messages/Messages.tsx';
+
 function App() {
   return (
-    <div>Hello, world</div>
+    <>
+      <header>
+        <AppToolbar/>
+      </header>
+      <main>
+        <Container maxWidth="xl">
+          <Routes>
+            <Route path="/" element={<Messages />} />
+            <Route path="*" element={<Typography variant="h1">Not found!</Typography>} />
+          </Routes>
+        </Container>
+      </main>
+    </>
   );
 }
 
